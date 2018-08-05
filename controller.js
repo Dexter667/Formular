ace530a1516ead2725a8628ac094d0c32452056f
Juraj Beger <juraj.beger@gmail.com>
2018-08-05 11:05:37 +0200

    zmena 2

---
+++ controller.js
@@ -0,0 +1,9 @@
+//Dátum
+
+/* $('document').ready(function(){
+    $('#inputDatumNarodenia').on('click', function () {
+        var end = $('#datetimepicker').val()
+        alert(end);
+    });
+)} */
+alert("Hello world");
